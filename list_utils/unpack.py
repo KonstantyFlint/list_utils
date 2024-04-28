431f0da1c9be6541d18142bde0030e2f4cc7b1ca
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2024-04-28 13:58:17 +0200

    ignoring unused arguments from structure

--- list_utils/unpack.py
+++ list_utils/unpack.py
@@ -1,3 +1,4 @@
+from inspect import getfullargspec
 from typing import Tuple, Callable, Dict, List, Any
 
 
@@ -20,7 +21,7 @@ def _get_name_to_address(structure: Tuple) -> Dict[str, List[int]]:
     return indexes
 
 
-def _get_name_to_value(name_to_address, obj) -> Dict[str, Any]:
+def _get_name_to_value(name_to_address: Dict[str, Any], obj) -> Dict[str, Any]:
     def get_value(address, obj_):
         for index in address:
             obj_ = obj_[index]
@@ -34,8 +35,29 @@ def _get_name_to_value(name_to_address, obj) -> Dict[str, Any]:
     return named_values
 
 
+def _get_arg_names(structure: Tuple):
+    names = set()
+    for value in structure:
+        match value:
+            case str() as name:
+                names.add(name)
+            case tuple() as nested:
+                names = names | _get_arg_names(nested)
+            case None:
+                pass
+            case bad_type:
+                raise NotImplementedError(f"expected a tuple, str or None, got: {bad_type}")
+    return names
+
+
 def unpack(structure: Tuple, fun: Callable):
+    used_args = getfullargspec(fun).args
     name_to_address = _get_name_to_address(structure)
+    name_to_address = {
+        name: address
+        for (name, address) in name_to_address.items()
+        if name in used_args
+    }
 
     def wrapper(obj):
         name_to_value = _get_name_to_value(name_to_address, obj)
